70a52cd153b9c425d2e912e64b1ddf1e2225d4fe
leonnwosu <nwosuleon@gmail.com>
2024-07-01 17:02:18 -0500

    page 2 change

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,16 +7,16 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>leon</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">leon na master</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="welcome to my world" />
       </IonContent>
     </IonPage>
   );
