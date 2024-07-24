606eeede32b3787311108b993c79219cc24b0dbb
leon nwosu <nwosuleon@gmail.com>
2024-07-24 02:00:53 -0500

    homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,70 +1,16 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
-import ExploreContainer from "../../components/ExploreContainer";
-import "./Home.css";
-import React, { useEffect, useState } from "react";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../../components/ExploreContainer';
+import './Home.css';
+
 
 const Home: React.FC = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
-  const [affirmation, setAffirmation] = useState(""); // Declare the state for affirmation
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
-
-  const dayNames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  const day = dayNames[currentTime.getDay()];
-  const daynum = currentTime.getDate();
-  const month = monthNames[currentTime.getMonth()];
-  const time = currentTime.toLocaleTimeString([], {
-    hour: '2-digit',
-    minute: '2-digit',
-  });
-  return (
-    <IonPage>
-      <IonContent fullscreen color="primary">
-      <div className="container">
-          <div className="display-time text-5xl text-white">{time}</div>
-          <div className="display-date text-white">
-            <span id="day">{day}</span>, <span id="daynum">{daynum}</span>{" "}
-            <span id="month">{month}</span>
-          </div>
-        </div>
-      </IonContent>
-    </IonPage>
-  );
-};
-
-export default Home;
+    return (
+      <IonPage>
+        <IonContent fullscreen color='primary'>
+          
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Home;
