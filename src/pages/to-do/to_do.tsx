d58682dbacd942f3d45414a09204d82c43223b53
leon nwosu <nwosuleon@gmail.com>
2024-07-29 02:35:26 -0500

    to-do page function implementation

--- src/pages/to-do/to_do.tsx
+++ src/pages/to-do/to_do.tsx
@@ -13,16 +13,68 @@ import {
 import ExploreContainer from "../../components/ExploreContainer";
 import "./to_do.css";
 import React, { useEffect, useState } from "react";
+interface ToDoItem {
+  text: string;
+  completed: boolean;
+}
 
 const ToDo: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<string>("");
+  const [newToDo, setNewToDo] = useState<string>("");
+  const [toDoItems, setToDoItems] = useState<{ [key: string]: ToDoItem[] }>({});
   const handleDateChange = (e: CustomEvent) => {
     setSelectedDate(e.detail.value);
   };
 
-  const Addtolist = () => {};
-  const complete = () => {};
-  const not_complete = () => {};
+  const Addtolist = () => {
+    if (newToDo.trim()) {
+      setToDoItems((prevItems) => {
+        const itemsForDate = prevItems[selectedDate] || [];
+        return {
+          ...prevItems,
+          [selectedDate]: [
+            ...itemsForDate,
+            { text: newToDo, completed: false },
+          ],
+        };
+      });
+      setNewToDo("");
+    }
+  };
+  const complete = () => {
+    if (toDoItems[selectedDate]) {
+      let complete = [];
+      for (let x = 0; x < toDoItems[selectedDate].length; x++) {
+        if (toDoItems[selectedDate][x].completed) {
+          complete.push(toDoItems[selectedDate][x]);
+        }
+      }
+      if (complete != null) {
+        return complete;
+      } else {
+        return [];
+      }
+    }
+  };
+  const not_complete = () => {
+    if (toDoItems[selectedDate]) {
+      let notcomplete = [];
+      for (let x = 0; x < toDoItems[selectedDate].length; x++) {
+        if (!toDoItems[selectedDate][x].completed) {
+          notcomplete.push(toDoItems[selectedDate][x]);
+        }
+      }
+      if (complete != null) {
+        return complete;
+      } else {
+        return [];
+      }
+    }
+  };
+
+  const makedone = ()=>{
+    
+  }
   return (
     <IonPage>
       <IonContent fullscreen color="primary">
@@ -39,11 +91,18 @@ const ToDo: React.FC = () => {
         </IonModal>
         <div className="showbutton">
           <IonButton
-            onClick={complete} color="favorite"  className="completebutton">
+            onClick={complete}
+            color="favorite"
+            className="completebutton"
+          >
             complete
           </IonButton>
 
-          <IonButton onClick={not_complete} color="favorite" className="notcomplete" >
+          <IonButton
+            onClick={not_complete}
+            color="favorite"
+            className="notcomplete"
+          >
             not complete
           </IonButton>
         </div>
